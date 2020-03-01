a7eca9ce5265cab328194304463956ef31085dc1
electricSoda <justin.hua.ge@gmail.com>
2020-03-01 12:01:02 -0700

    Collab Session1 on VSCode

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 var counter = 0;
+
 var count = (function () {
     return function () { return counter += 1; }
 })();
 
 function displaycount() {
     document.getElementById("carrier").innerHTML = count();
+}
+
+function open() {
+    var myBody = document.getElementsByTagName("body");
+    myBody[0].innerHTML = "<p> Hi </p>";
 }
